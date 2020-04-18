f07c6f4491d660628368b7ba9903df812a21ab45
Samuel-dot-cloud <successam336@gmail.com>
2020-04-18 15:44:42 +0300

    Addition of a constructor and custom class to model

---
+++ src/main/java/models/Class.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Class {
+private String Statement;
+private int Key;
+
+public Class (String Statement, int Key){
+    this.Statement = Statement;
+    this.Key = Key;
+}
+}
