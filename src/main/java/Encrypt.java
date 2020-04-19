e070ece965623cd2221042104000094d76954de1
Samuel-dot-cloud <successam336@gmail.com>
2020-04-19 16:54:43 +0300

    Definition of encryption file

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,5 +1,49 @@
-public class Encrypt{
+public class Encrypt {
     private int Key;
     private String Statement;
-    private String Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String lowercase = "abcdefghijklmnopqrstuvwxyz";
+    private String special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    private String numbers = "0123456789";
+    private char ch;
+    private String encryptedMessage = "";
+
+
+    public String Encrypt(String Statement, int Key) {
+        this.Statement = Statement.toUpperCase();
+        this.Key = Key;
+        if(Key>26){
+            Key=Key%26;
+        }else{
+            Key = (Key%26)+26;
+        }
+        for(int i=0;i<Statement.length();i++)
+        {
+                char newCharacter = Statement.charAt(i);
+            if (Character.isLetter(newCharacter)){
+                if (Character.isLowerCase(newCharacter)) {
+                    char c = (char) (newCharacter + Key);
+                    if (c > 'z') {
+                        encryptedMessage = encryptedMessage + (char) (newCharacter - (26 - Key));
+                    } else {
+                        encryptedMessage = encryptedMessage + c;
+                    }
+
+
+                    } else if(Character.isUpperCase(newCharacter)){
+                    char c = (char) (newCharacter + Key);
+                    if (c >'Z') {
+                        encryptedMessage = encryptedMessage + (char) (newCharacter - (26 - Key));
+                    } else {
+                        encryptedMessage = encryptedMessage + c;
+                    }
+                }
+                else{
+                    encryptedMessage = encryptedMessage + newCharacter;
+                }
+            }
+        }
+
+        return encryptedMessage;
+    }
 }
--- src/main/java/models/Class.java
+++ src/main/java/models/Class.java
@@ -7,5 +7,5 @@ private int Key;
 public Class (String Statement, int Key){
     this.Statement = Statement.toUpperCase();
     this.Key = Key;
-}
+ }
 }
